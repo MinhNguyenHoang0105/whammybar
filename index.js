a21834cd9f794009c80aa96200a0111c9bc5225b
dokyan1989 <dokyan1989@gmail.com>
2018-04-04 11:56:38 +0700

    fix port lan 2

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.listen(process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.get("/", function(req, res){
 	res.render("trangchu");
